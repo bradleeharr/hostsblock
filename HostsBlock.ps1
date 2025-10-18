e68468633f143d18f3a25b54c91916d88fa5ee8c
Bradlee Harrison <bradlee.harrison22@gmail.com>
2025-10-18 01:33:07 -0500

    help function ver 2

--- HostsBlock.ps1
+++ HostsBlock.ps1
@@ -1,21 +1,37 @@
 param (
     [string[]]$block,
-    [string[]]$unblock
+    [string[]]$unblock,
+    [string]$outputFile
 )
+$hostsFilePath = "C:\Windows\System32\drivers\etc\hosts"
 function _help() {
-    Write-Host " Block Hosts File "
-    Write-Host " $block_entries   : Block Entries"
-    Write-Host " $unblock_entries  : Unblock Entires"
+    Write-Host "HostsBlock.ps1 -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- "
+    Write-Host " | A script utility for adding lines to block websites via hosts file / remove entries on host file by url "
+    Write-Host " |  --block : List of entries to block"
+    Write-Host " |  --unblock : List of entries to unblock"
+    Write-Host " |  --outputFile : (Optional) the path for the hosts file to write to."
+    Write-Host " |                 Otherwise will write to the path at '$hostsFilePath'"
+    Write-Host " |  -h / --help : Print help"
+    Write-Host " |  -l / --list : List host file content"
+    Write-Host "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- "
 }
 if ("-h" -in $args) {
     _help;
     exit
 }
-
+if ("-l" -in $args -or "--list" -in $args) {
+    Write-Host "List in args"
+    Get-Content $hostsFilePath
+    exit
+}
+if (!$outputFile) {
+    Write-Host "Output File Empty, Saving to Default"
+    $outputFile = "hosts.txt"
+}
+Out-File $outputFile -Encoding UTF8
 
 $block_entries = @("--block", "-b")
 $unblock_entries = @("--unblock", "-u")
-$hostsFilePath = "C:\Windows\System32\drivers\etc\hosts"
 
 function remove_entry($url) {
 
