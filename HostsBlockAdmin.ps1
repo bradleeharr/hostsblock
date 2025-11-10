40b751daa5bc343f761e76b71aec76f90623f37a
Bradlee Harrison <bradlee.harrison22@gmail.com>
2025-11-09 19:19:03 -0600

    Adjust HostsBlockAdmin.ps1 to only do "noExit" when "--list" is in args

--- HostsBlockAdmin.ps1
+++ HostsBlockAdmin.ps1
@@ -1,5 +1,12 @@
 Write-Host "This script runs the HostsBlock.ps1, requesting Admin permissions."
 Write-Warning "(Warning) This script runs the HostsBlock.ps1, requesting Admin permissions."
 
+
 Write-Host "Args : $args"
-Start-Process powershell.exe  -Verb RunAs -ArgumentList "-NoExit -File $PSScriptRoot\HostsBlock.ps1 $args"  
+
+if ("--list" -in $args) {
+   Start-Process powershell.exe  -Verb RunAs -ArgumentList "-NoExit -File $PSScriptRoot\HostsBlock.ps1 $args"
+}
+else {
+   Start-Process powershell.exe  -Verb RunAs -ArgumentList "-File $PSScriptRoot\HostsBlock.ps1 $args"
+}
