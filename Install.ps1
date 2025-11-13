c5660905cd6a358a8d26697cd402eb675bebb932
Bradlee Harrison <bradlee.harrison22@gmail.com>
2025-11-12 23:37:48 -0600

    Update install script to use script root dir instead of hardcoded value

--- Install.ps1
+++ Install.ps1
@@ -1,2 +1,4 @@
 
-Add-Content -Path $PROFILE -Value '`nSet-Alias -Name HBlock -Value "C:\Users\bubba\Desktop\hostsblock\HostsBlockAdmin.ps1"'
+$Script = "$PSScriptRoot\HostsBlockAdmin.ps1"
+Write-Host $Script
+Add-Content -Path $PROFILE -Value "`nSet-Alias -Name HBlock -Value $Script"
