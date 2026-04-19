4cb56df775f0c01ea5a46e10db17fe40dd964488
bradleeharr <bradlee.harrison@git.com>
2026-04-19 12:51:14 -0500

    Create Windows powershell profile if it doesn't exist

--- Install.ps1
+++ Install.ps1
@@ -1,4 +1,15 @@
 
+
+
 $Script = "$PSScriptRoot\HostsBlockAdmin.ps1"
 Write-Host $Script
+
+
+Write-Host "Adding HBlock to Profile..."
+if (!(Test-Path -Path $PROFILE)) {
+    Write-Host "    Profile Does not Exist... Creating new File $PROFILE"
+    New-Item -ItemType File -Path $PROFILE
+}
+
+Write-Host "    Adding item to Profile..."
 Add-Content -Path $PROFILE -Value "`nSet-Alias -Name HBlock -Value $Script"
